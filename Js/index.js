eea75405bdc1b66eea5e7012f2c4d1cab4ac343c
Resul <resulhesenli02@gmail.com>
2022-11-12 01:00:26 +0400

    first commit

---
+++ Js/index.js
@@ -0,0 +1,24 @@
+"use strict";
+//ok отмена
+// const result = confirm("Are you here?");
+// console.log(result);
+
+//ввод цифр(возраста)
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer) + ' ' + answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", '');
+// answers[1] = prompt("Как вашa фамилия?", '');
+// answers[2] = prompt("Сколько вам лет?", '');
+
+// document.write(answers);
+
+// const answers = [prompt("Как ваше имя?", ''), prompt("Как вашa фамилия?", ''), prompt("Сколько вам лет?", '')];
+
+// document.write(answers);
+
+    // const category = 'toys';
+
+    // console.log(`https://someurl.com/${category}/5`);
