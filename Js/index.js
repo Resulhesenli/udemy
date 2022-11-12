c81ddf7f61be787a389045fed7e70d41ff009254
Resul <resulhesenli02@gmail.com>
2022-11-12 19:28:04 +0400

    console program

--- Js/index.js
+++ Js/index.js
@@ -22,3 +22,27 @@
     // const category = 'toys';
 
     // console.log(`https://someurl.com/${category}/5`);
+
+    let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+    
+    
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+    
+    let lastMovie = prompt("Один из последних просмотренных фильмов?", "");
+    let movieIdb = +prompt("Насколько оцените его?", "");
+    
+    personalMovieDB.movies[lastMovie] = movieIdb;
+
+    console.log(personalMovieDB);
+
+   
+    // movies['lastmovie' + ':' + 'movieIdb'];
+
+
+    // console.log(personalMovieDB);
